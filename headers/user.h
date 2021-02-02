934855753e4abbbb7c8898654916bd376ef77a91
yudinghong <yudinghong_zq@outlook.com>
2021-02-02 17:01:35 +0800

    update

--- headers/user.h
+++ headers/user.h
@@ -1,10 +1,13 @@
 #ifndef _USER
 #define _USER
 
+#include "linklist.h"
+
 typedef struct {
   char account[10];
   char password[16];
-
+  int priority;
+  Node node;
 }User, *PUser;
 
 void analyseUser(const char *fileName);
--- sources/user.c
+++ sources/user.c
@@ -1,6 +1,8 @@
 #include "../headers/user.h"
 #include <stdio.h>
 
+static PNode userHead = NULL;
+
 void analyseUser(const char *fileName) {
   FILE *file = fopen(fileName, "r");
   
